680ec3c73e070370bfbadb7f22d8b487284de4c3
Diego Medrano <dmorales@arkusnexus.com>
2021-07-07 09:58:25 -0500

    show activity lifecycle components & mvvm arch

--- app/src/main/java/com/arksunexus/bootcampcolombia/MainActivity.kt
+++ app/src/main/java/com/arksunexus/bootcampcolombia/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.arksunexus.bootcampcolombia
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.arksunexus.bootcampcolombia.databinding.ActivityMainBinding
 
@@ -16,12 +19,38 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("BootcampColombia", "Lifecycle: onCreate")
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
         setupListView()
         setupButton()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("BootcampColombia", "Lifecycle: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("BootcampColombia", "Lifecycle: onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("BootcampColombia", "Lifecycle: onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("BootcampColombia", "Lifecycle: onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("BootcampColombia", "Lifecycle: onDestroy")
     }
 
     private fun setupListView() {
